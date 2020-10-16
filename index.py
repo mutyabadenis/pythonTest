de18b4890194dbf2753e5569aa5e505936676256
mutyaba denis <denismutyaba4@gmail.com>
2020-10-16 10:07:56 -0700

    My first commit

---
+++ index.py
@@ -0,0 +1,12 @@
+def sum_of_nums ( *hobbies):
+   hobbyList = []
+   for hobby in hobbies:
+       hobbyList.insert(0,hobby)
+
+       print(hobbyList)
+
+sum_of_nums('swimming', 'football', 'watching Movies')
+ 
+
+
+    
